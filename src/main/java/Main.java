d503c2d58ee9a11380b5b4e9c2ed08a7a7526b29
Serhii iLchenko <silchenko@agileassets.com>
2022-04-15 20:19:06 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("My window");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(0, 0, 300, 300);
+
+    frame.setVisible(true);
+  }
+}
